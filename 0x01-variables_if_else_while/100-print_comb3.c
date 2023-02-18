037d649e1240ab87bc6ae91615f84b79ac74bef6
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-18 06:12:05 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,9 @@ int main(void)
 	int j;
 
 	for (i = 48; i <= 57; i++)
-	{
-		putchar(i);
-		for (j = 48; j <= 57; j++)
+		for (j = i;;)
 		{
-			putchar(j);
-			if (i != 57 && j != 57)
+			if (i != 57)
 			{
 				putchar(',');
 				putchar(' ');
