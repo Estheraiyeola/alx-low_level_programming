29ef6fa4625c5138195ca1d65272181259f242f6
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-17 16:49:29 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - a program printing single digit numbers 0-9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d" , num);
+	printf("\n");
+	return (0);
+}
