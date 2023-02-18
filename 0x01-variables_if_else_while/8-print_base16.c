e6278d3e1d5d9ced8d693ab7484e042db397c617
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-17 17:59:11 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - printing numbers in base 16
+ * Returns: Always 0
+ */
+int main(void)
+{
+	int num;
+	char c;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar ('\n');
+	return (0);
+}
