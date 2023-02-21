78b23a47bf5d2d7615cb1b4a194135a2f16183a4
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-21 05:41:47 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
-#include<stdio.h>
 #include<main.h>
 /**
  * main - print _putchar
  *
  * Return: Always 0
  */
-int main(void)
+int _putchar(char _putchar)
 {
-	putchar("_putchar");
-	putchar("\n");
 	return (0);
 }
