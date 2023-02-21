480c1e41aebaaf3ca2a5b6c5ec89b443347795dd
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-21 10:50:37 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _islower - check for lowercases
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
