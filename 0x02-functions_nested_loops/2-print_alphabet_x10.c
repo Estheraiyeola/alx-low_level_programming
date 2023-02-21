c1415fb527d1fba690b74ddac4104fc8a5800065
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-21 08:54:22 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - printing lowercase alphbets ten times
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar ('\n');
+}
