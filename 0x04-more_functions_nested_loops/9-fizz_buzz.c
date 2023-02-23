a7e2d986c44a65e23fb2a3508b2ce793adcb004e
Estheraiyeola <estheraiyeola@yahoo.com>
2023-02-23 11:58:43 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - prints from 1 to 100
+ * Return: Always 0
+ */
+int main()
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else
+		{
+			(printf("%d\n", i);
+		}
+		
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+}
