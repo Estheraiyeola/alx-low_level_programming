d5c42bbf3befe2ac067e7d7e6233c910d20bf89c
Estheraiyeola <estheraiyeola@yahoo.com>
2023-03-02 00:17:31 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr = dest;
+	int i;
 
-	while ((*ptr++ = *src++) != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		continue;
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
