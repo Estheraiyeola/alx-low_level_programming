3f9a7540a2c7f081af6a8dad6dc899962d87d6da
Estheraiyeola <estheraiyeola@yahoo.com>
2023-03-02 14:57:21 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: the string to be encoded
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
+			{
+				s[i] = s[i] - 13;
+			}
+			s[i] = s[i] + 13;
+		}
+		i++;
+	}
+	return (s);
+}
