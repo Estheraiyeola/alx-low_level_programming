486579cc75dad270ca44b53aa33fff3660cbf7b0
Estheraiyeola <estheraiyeola@yahoo.com>
2023-03-03 18:45:38 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,13 +3,11 @@
 /**
  * print_number - prints an integer
  * @n: the number to be printed
- * Return: 0
+ * Return: empty
  */
 void print_number(int n)
 {
-	int *i;
-
 	int *i = &n;
+
 	_putchar(*i);
-	return (0);
 }
