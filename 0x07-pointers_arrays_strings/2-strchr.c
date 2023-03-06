d123247993c683d604ca5cffc1aaa5a9e6ed555d
Estheraiyeola <estheraiyeola@yahoo.com>
2023-03-06 04:22:33 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _strchr - locates a character string
+ * @s: a pointer to the null terminated string
+ * @c: the character to be searched for in string s
+ * Return: s, if c is found , NULL if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
