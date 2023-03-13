04b4bae4f9f49ee271c4e95a484f9f0311e171bc
Estheraiyeola <estheraiyeola@yahoo.com>
2023-03-13 09:17:28 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,18 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *a = (char *) malloc(size * sizeof(char));
+	char *a;
 
 	if (size == 0)
 	{
 		return ('\0');
+	}
+	
+	a = malloc(size * sizeof(char));
+
+	if (a == NULL)
+	{
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
